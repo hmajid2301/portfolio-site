b790c6b822fe25ad70a805f48257ac474304f2bd
Haseeb Majid <hello@haseebmajid.dev>
2020-04-03 23:27:45 +0100

    Update Repos
    Update GitHub repos shown on the website.

--- src/data/GithubRepos.ts
+++ src/data/GithubRepos.ts
@@ -4,39 +4,39 @@ const GithubRepos = [
     url: "https://github.com/hmajid2301/articles",
     description:
       "This repo contains all the articles written by me, in markdown.",
-    language: "Python"
+    language: "Python",
   },
   {
     name: "hmajid2301/dockerhub-descriptions-updater",
     url: "https://github.com/hmajid2301/dockerhub-description-updater",
     description:
       "Docker Image used to update the description on Docker Hub repos.",
-    language: "JavaScript"
+    language: "JavaScript",
   },
   {
     name: "hmajid2301/dind-docker-compose",
     url: "https://github.com/hmajid2301/dind-docker-compose",
     description: "Docker Compose running in a Docker image.",
-    language: "Dockerfile"
+    language: "Dockerfile",
   },
   {
     name: "hmajid2301/gitlab-auto-release",
     url: "https://github.com/hmajid2301/gitlab-auto-release",
     description: "Python script which is used to create releases on GitLab.",
-    language: "Python"
+    language: "Python",
   },
   {
-    name: "hmajid2301/python-cli-cookiecutter",
-    url: "https://github.com/hmajid2301/python-cli-cookiecutter",
-    description: "Cookie Cutter template for a simple Python CLI project.",
-    language: "Python"
+    name: "hmajid2301/markdown-mermaid-to-images",
+    url: "https://github.com/hmajid2301/markdown-mermaid-to-images",
+    description: "Exports mermaid diagrams in Markdown documents as images.",
+    language: "Python",
   },
   {
     name: "hmajid2301/stegappasaurus-api",
     url: "https://github.com/hmajid2301/stegappasaurus-api",
     description: "Firebase Cloud Functions for the stegappasaurus app.",
-    language: "TypeScript"
-  }
+    language: "TypeScript",
+  },
 ];
 
 export default GithubRepos;
