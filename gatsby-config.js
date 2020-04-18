e45e1b2a99140eae705961bb622c6e05d4f8167a
Haseeb Majid <hello@haseebmajid.dev>
2020-04-19 00:15:42 +0100

    fix: Edit Manifest
    Edit the manifest with correct information.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Haseeb Majid | Software Developer`,
+    description: `Haseeb Majid's personal website.`,
+    author: `Haseeb Majid`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,20 +18,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Haseeb Majid`,
+        short_name: `Haseeb`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-typescript`,
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-offline`,
   ],
 }
