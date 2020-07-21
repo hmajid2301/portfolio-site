fdc3bda5ce1ea8ce7b232550f438e1599e434692
Haseeb Majid <hello@haseebmajid.dev>
2020-07-21 14:37:55 +0100

    feat: Add 404 Page
    Added a basic 404 page to the app.

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '~/components/Layout';
+
+const PageNotFound = () => (
+  <Layout>
+    <h1>404</h1>
+  </Layout>
+);
+
+export default PageNotFound;
