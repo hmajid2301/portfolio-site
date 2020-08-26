abba4de5b5f8102889133585115803a7257223d6
Haseeb Majid <hello@haseebmajid.dev>
2020-08-26 15:02:20 +0100

    work

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -44,8 +44,9 @@ const Stats = ({ data }: Props) => {
   const totalPosts = blogItems.length;
   const {
     dayData,
-    monthData,
     dayWordData,
+    monthData,
+    monthWordData,
     totalWords,
     totalMinutes,
   } = getBlogStats(blogItems);
@@ -54,6 +55,7 @@ const Stats = ({ data }: Props) => {
   const days: ChartData[] = objectToDataArray(dayData, totalPosts);
   const months: ChartData[] = objectToDataArray(monthData, totalPosts);
   const dayWords: ChartData[] = objectToDataArray(dayWordData, totalWords);
+  const monthWords: ChartData[] = objectToDataArray(monthWordData, totalWords);
   const popularPosts: ChartData[] = objectToDataArray(popularData, totalViews);
 
   const hours = Math.floor(totalMinutes / 60);
@@ -75,13 +77,14 @@ const Stats = ({ data }: Props) => {
           total={totalWords}
           unit="words"
         />
-        <HorizontalBarChart data={days} title="Posts per Day" />
-        <HorizontalBarChart data={dayWords} title="Words per Day" />
-        <VerticalBarChart data={months} title="Posts per Month" />
         <HorizontalBarChart
           data={popularPosts}
           title="Most Popular Blog Posts"
         />
+        <HorizontalBarChart data={days} title="Posts per Day" />
+        <HorizontalBarChart data={dayWords} title="Words per Day" />
+        <VerticalBarChart data={months} title="Posts per Month" />
+        <HorizontalBarChart data={monthWords} title="Words per Month" />
       </Section>
     </Layout>
   );
@@ -125,6 +128,21 @@ function getBlogStats(blogItems: Props['data']['allMarkdownRemark']['edges']) {
     Dec: 0,
   };
 
+  const monthWordData: { [name: string]: number } = {
+    Jan: 0,
+    Feb: 0,
+    Mar: 0,
+    Apr: 0,
+    May: 0,
+    Jun: 0,
+    Jul: 0,
+    Aug: 0,
+    Sep: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0,
+  };
+
   const dayWordData: { [name: string]: number } = {
     Monday: 0,
     Tuesday: 0,
@@ -146,10 +164,18 @@ function getBlogStats(blogItems: Props['data']['allMarkdownRemark']['edges']) {
     const [day, month] = date.split(' ');
     dayData[day] += 1;
     monthData[month] += 1;
+    monthWordData[month] += words;
     dayWordData[day] += words;
   });
 
-  return { dayData, monthData, dayWordData, totalWords, totalMinutes };
+  return {
+    dayData,
+    monthData,
+    monthWordData,
+    dayWordData,
+    totalWords,
+    totalMinutes,
+  };
 }
 
 function objectToDataArray(obj: Record<string, number>, total: number) {
@@ -165,7 +191,7 @@ function objectToDataArray(obj: Record<string, number>, total: number) {
   return newArr;
 }
 
-const Section = tw.section`max-w-xl mx-auto my-10 px-4`;
+const Section = tw.section`max-w-2xl mx-auto my-10 px-4`;
 
 const MainText = tw.p`text-main text-lg font-body bg-secondary-background p-4`;
 
